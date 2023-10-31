3dd0f2f51779e73b1b27e58663573bc80947b75d
fractal-robot <utsdxbur@proton.me>
2023-10-31 12:28:57 +0100

    2021-04 - adding interpretation of the boards

--- 2021/04/part-1/main.cpp
+++ 2021/04/part-1/main.cpp
@@ -1,5 +1,7 @@
 // using negative number to avoid creating boolean 2 dimentionnal arrays
 
+#include <array>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -7,7 +9,10 @@
 #include <string_view>
 #include <vector>
 
-using boards = std::vector<std::vector<std::vector<int>>>;
+constexpr std::size_t boardSize{5};
+
+using Board = std::array<std::array<int, boardSize>, boardSize>;
+using BoardsVector = std::vector<Board>;
 
 // can't use std::string_view with constructor of std::istringstream
 std::vector<int> &getValuesArray(const std::string &strInput) {
@@ -24,7 +29,56 @@ std::vector<int> &getValuesArray(const std::string &strInput) {
   return values;
 }
 
-void getBingoBoards(const std::string &strInput) { ; }
+// can't use std::string_view with constructor of std::istringstream
+std::array<int, boardSize> stringToArray(const std::string &strInput) {
+
+  std::array<int, boardSize> array{};
+  std::istringstream iss(strInput);
+
+  for (std::size_t i = 0; i < boardSize; i++) {
+    iss >> array[i];
+  }
+
+  for (int i : array) {
+    std::cout << i << ' ';
+  }
+
+  std::cout << '\n';
+
+  return array;
+}
+
+void printBoards(const BoardsVector &boards) {
+
+  for (const Board &board : boards) {
+    for (const auto &arow : board) {
+      for (const auto &e : arow) {
+        std::cout << e << '\t';
+      }
+      std::cout << '\n';
+    }
+    std::cout << '\n';
+  }
+}
+
+void getBingoBoards(const std::string &strInput, Board &board,
+                    BoardsVector &boards) {
+  static std::size_t counter;
+  static Board voidBoard{};
+
+  if (strInput.empty()) {
+    if (board != voidBoard) {
+      counter = 0;
+      // so a new line is necessary at the end of the input file
+      boards.push_back(board);
+      board = voidBoard;
+    }
+  } else {
+    board[counter] = stringToArray(strInput);
+
+    ++counter;
+  }
+}
 
 int main() {
 
@@ -38,15 +92,23 @@ int main() {
 
   std::vector<int> *values;
 
+  static BoardsVector boards{};
+  boards.reserve(50);
+
+  static Board board{0};
+
   while (std::getline(inf, strInput)) {
     if (firstI) {
       values = &getValuesArray(strInput);
       firstI = false;
-
     } else {
-      getBingoBoards(strInput);
+      // change board and boards
+      getBingoBoards(strInput, board, boards);
     }
   }
+
+  std::cout << "\n\n\n";
+  printBoards(boards);
 
   for (int i : *values) {
     std::cout << i << ' ';
