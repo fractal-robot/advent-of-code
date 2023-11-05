abd832f94224469ca0bc5775f5db9366de309db8
fractal-robot <utsdxbur@proton.me>
2023-11-05 10:48:12 +0100

    2021 - 08 - part-1 - took me a LOT of time to understand the concept - finished

--- 2021/08/part-1/main.cpp
+++ 2021/08/part-1/main.cpp
@@ -1,15 +1,22 @@
+// 1: 2
+// 4: 4
+// 7: 3
+// 8: 7
+
 #include <cstddef>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <sstream>
 #include <string>
 #include <utility>
 
-std::pair<std::string, std::string> &splitString(const std::string &str) {
+std::pair<std::string, std::string> splitString(const std::string &str) {
 
   std::string left{str.substr(0, str.find('|'))};
-  std::string right{str.substr(str.find('|') + 2)};
-  static std::pair<std::string, std::string> splittedString{left, right};
+  std::string right{str.substr(str.find('|') + 1)};
+  std::pair<std::string, std::string> splittedString{left, right};
 
   return splittedString;
 }
@@ -20,10 +27,22 @@ int main() {
     std::cerr << "unable to open file.";
   }
 
-  std::string strinput;
-  while (std::getline(inf, strinput)) {
-    std::pair<std::string, std::string> splittedstring{splitString(strinput)};
+  std::string strInput;
+  int counter{};
 
-    std::cout << splittedstring.first << '\n' << splittedstring.second;
+  while (std::getline(inf, strInput)) {
+    std::pair<std::string, std::string> splittedstring{splitString(strInput)};
+    std::istringstream iss{splittedstring.second};
+    std::string token;
+
+    while (std::getline(iss, token, ' ')) {
+      size_t strSize = std::size(token);
+      if ((strSize >= 2 && strSize <= 4) || strSize == 7) {
+        ++counter;
+      }
+    }
   }
+
+  std::cout << counter;
+  return EXIT_SUCCESS;
 }
