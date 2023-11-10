e78af28fe0f26a4bffb3c7633e3bd77be6b1e7e8
fractal-robot <utsdxbur@proton.me>
2023-11-10 10:38:23 +0100

    2022 - 06 - part-2 - finished

--- 2022/05/part-2/main.cpp
+++ 2022/05/part-2/main.cpp
@@ -9,29 +9,24 @@ int main() {
   std::ifstream inf{"input.txt"};
   if (!inf) {
     std::cerr << "Unable to open file.";
-    return 1;
   }
 
   std::string strInput;
-  short counter{};
 
   while (std::getline(inf, strInput, '\n')) {
     for (std::size_t i{0}; i < std::size(strInput); ++i) {
       std::unordered_set<char> charSet;
 
-      // Use a loop to insert characters into charSet
-      for (std::size_t j{i}; j < i + 4; ++j) {
+      for (std::size_t j{i}; j < i + 14; ++j) {
         charSet.insert(strInput[j]);
       }
 
-      if (std::size(charSet) == 4) {
-        std::cout << i + 4 << '\n';
-        ++counter;
+      if (std::size(charSet) == 14) {
+        std::cout << i + 14 << '\n';
         break;
       }
     }
   }
 
-  std::cout << '\n' << counter;
   return 0;
 }
