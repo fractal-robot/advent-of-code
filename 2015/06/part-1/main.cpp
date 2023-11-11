cce7bf5bea129d0c980ff1aaaf345e3134f6e2fb
fractal-robot <utsdxbur@proton.me>
2023-11-11 14:42:10 +0100

    2015 - 06 - part-2 - finished

--- 2015/06/part-1/main.cpp
+++ 2015/06/part-1/main.cpp
@@ -19,7 +19,7 @@ int main() {
       "(turn on|turn off|toggle) ([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)");
   std::smatch matches;
 
-  std::array<std::array<bool, 1000>, 1000> grid{};
+  std::array<std::array<int, 1000>, 1000> grid{};
 
   while (std::getline(inf, strInput, '\n')) {
     if (std::regex_search(strInput, matches, pattern)) {
@@ -29,31 +29,29 @@ int main() {
       std::pair<size_t, size_t> endingCoord{std::stoul(matches[4]),
                                             std::stoul(matches[5])};
 
-      std::cout << action << ' ' << startingCoord.first << ", "
-                << startingCoord.second << " - " << endingCoord.first << ", "
-                << endingCoord.second << '\n';
-
       // I don't know if there's any compiler optimization that allow to
       // remember the result of a condition through multiple loops
       if (action == "turn on") {
         for (std::size_t i{startingCoord.first}; i <= endingCoord.first; ++i) {
           for (std::size_t j{startingCoord.second}; j <= endingCoord.second;
                ++j) {
-            grid[i][j] = true;
+            ++grid[i][j];
           }
         }
       } else if (action == "turn off") {
         for (std::size_t i{startingCoord.first}; i <= endingCoord.first; ++i) {
           for (std::size_t j{startingCoord.second}; j <= endingCoord.second;
                ++j) {
-            grid[i][j] = false;
+            if (grid[i][j] > 0) {
+              --grid[i][j];
+            }
           }
         }
       } else if (action == "toggle") {
         for (std::size_t i{startingCoord.first}; i <= endingCoord.first; ++i) {
           for (std::size_t j{startingCoord.second}; j <= endingCoord.second;
                ++j) {
-            grid[i][j] = !(grid[i][j]);
+            grid[i][j] += 2;
           }
         }
       }
@@ -64,11 +62,9 @@ int main() {
   }
 
   int counter{};
-  for (std::array<bool, 1000> row : grid) {
-    for (bool light : row) {
-      if (light) {
-        ++counter;
-      }
+  for (std::array<int, 1000> row : grid) {
+    for (int light : row) {
+      counter += light;
     }
   }
   std::cout << counter;
