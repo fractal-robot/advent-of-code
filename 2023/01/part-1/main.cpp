98e19fbfc53b96b81457983c4d5265af938ecd1d
fractal-robot <utsdxbur@proton.me>
2024-01-13 16:18:32 +0100

    2023-01-part-2

--- 2023/01/part-1/main.cpp
+++ 2023/01/part-1/main.cpp
@@ -1,9 +1,38 @@
+#include <array>
 #include <cctype>
+#include <cstddef>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <vector>
+
+std::vector<char> &findValues(const std::string &line) {
+  std::array<std::string, 9> numbers{
+      "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+  };
+
+  static std::vector<char> values{};
+  values.clear();
+  values.reserve(5);
+
+  for (std::size_t i{0}; i < std::size(line); ++i) {
+    if (std::isdigit(line[i])) {
+      values.push_back(line[i]);
+    }
+    for (std::size_t j{0}; j < std::size(numbers); ++j) {
+      int counter{0};
+      for (std::size_t k{0}; k < std::size(numbers[j]); ++k) {
+        if (i + k <= std::size(line) && line[i + k] == numbers[j][k])
+          ++counter;
+      }
+      if (counter == std::size(numbers[j]))
+        values.push_back(j + 1 + 48);
+    }
+  }
+  return values;
+}
 
 int main() {
   std::ifstream file{"input.txt"};
@@ -11,29 +40,14 @@ int main() {
     std::cerr << "Unable to open input file.";
   }
   std::string strInput;
-  int total{};
+  int sum{};
   while (std::getline(file, strInput, '\n')) {
-    std::string digits{};
-
-    for (char c : strInput) {
-      if (std::isdigit(c)) {
-        digits += c;
-        break;
-      }
-    }
-
-    for (auto c = strInput.rbegin(); c != strInput.rend(); ++c) {
-      if (std::isdigit(*c)) {
-        digits += *c;
-        break;
-      }
-    }
-
-    if (!digits.empty())
-      total += std::stoi(digits);
+    std::vector<char> values{findValues(strInput)};
+    std::string localSum{std::string(1, values[0]) +
+                         std::string(1, values[std::size(values) - 1])};
+    sum += std::stoi(localSum);
   }
 
-  std::cout << total;
-
+  std::cout << sum;
   return EXIT_SUCCESS;
 }
